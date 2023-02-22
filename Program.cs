48e9d7d0226d34c1cdf58fed51fc0aef30648066
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-22 22:21:51 +0530

    Largest of three

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using MinMaxProblems;
+
+namespace MaxMin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int max = MinMax.MaximumIntNum(30, 20, 10);
+            Console.WriteLine("{0} Greatest than Two ", max);
+        }
+    }
+}
