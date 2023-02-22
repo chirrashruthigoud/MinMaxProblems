58f9d1c7171793b64dc35bfdf8936a23708120c5
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-22 22:33:16 +0530

    String Max Min

--- Program.cs
+++ Program.cs
@@ -6,8 +6,12 @@ namespace MaxMin
     {
         public static void Main(string[] args)
         {
-            double max = MinMax.MaximumIntNum(1.8, 2.6, 3.4);
-            Console.WriteLine("{0} Greatest than Two ", max);
+            string str = "sunny";
+            string str1 = "siri";
+            string str2 = "Shruthi";
+
+            string Max = MinMax.MaximumIntNum(str, str1, str2);
+            Console.WriteLine(Max);
         }
     }
 }
