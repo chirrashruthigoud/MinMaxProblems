94f3e3f8083bf12ad4bc7902274ffc5fc851efa5
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-24 13:04:39 +0530

    Refactor to create Generic Class

--- Program.cs
+++ Program.cs
@@ -6,12 +6,13 @@ namespace MaxMin
     {
         public static void Main(string[] args)
         {
-            string str = "sunny";
-            string str1 = "siri";
-            string str2 = "Shruthi";
+            int a = MaxMin<int>.Maximum(10, 20, 30);
+            double b = MaxMin<double>.Maximum(10.20, 20.03, 30.2);
+            char c = MaxMin<char>.Maximum('A', 'C', 'B');
+            Console.WriteLine("Greatest Int : " + a);
+            Console.WriteLine("Greatest Double : " + b);
+            Console.WriteLine("Greatest Char : " + c);
 
-            string Max = MinMax.MaximumIntNum(str, str1, str2);
-            Console.WriteLine(Max);
         }
     }
 }
